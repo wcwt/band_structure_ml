adda4a533933c7261cf81441930c5f0091926915
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-17 23:36:25 +0800

    i

--- function_training.py
+++ function_training.py
@@ -1,5 +1,6 @@
 import torch
 import data_loader
+import pickle
 
 def train_one_epoch(device, model, optimizer, criterion, train_loader):
     model.train()
@@ -47,7 +48,11 @@ def validate_one_epoch(device, model, criterion, validate_loader):
 
 
 def validate_train_loop(device, model, optimizer, scheduler, criterion, validate_loader, train_loader,
-                        num_epoch, num_epoch_per_validate, state_dict_path,dataset=""):
+                        num_epoch, num_epoch_per_validate, state_dict_path,load_data = False):
+    if load_data:
+        with open ("data.pickle","rb") as f:
+            dataset = pickle.load(f)
+        validate_loader, train_loader = data_loader.get_validate_train_loader(dataset, 20)
     result = validate_one_epoch(device, model, criterion, validate_loader)
     print("\rvalidate loss:{} accuracy:{}%".format(*result))
     for epoch in range(num_epoch):
