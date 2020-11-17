24544d01433e1ed9ec82a05faf5e7681e6ce0bae
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-18 00:01:50 +0800

    change model type

--- main_cs2sg.py
+++ main_cs2sg.py
@@ -25,6 +25,17 @@ def main_one(csnum):
     output_size = cs_sizes[csnum - 1] - cs_sizes[csnum - 2] + 1 if csnum > 1 else 3
 
     model = torch.nn.Sequential(
+        #torch.nn.LeakyReLU(),
+        torch.nn.Linear(len(hs_indices)*num_bands, 64),
+        torch.nn.LeakyReLU(),
+        torch.nn.Linear(64, 32),
+        torch.nn.LeakyReLU(),
+        torch.nn.Linear(32, 7),
+        #torch.nn.LeakyReLU(),
+        #torch.nn.Softmax(dim=7),
+    )
+    """
+    model = torch.nn.Sequential(
         torch.nn.LeakyReLU(),
         torch.nn.Linear(len(hs_indices)*num_bands, 300),
         torch.nn.LeakyReLU(),
@@ -33,7 +44,7 @@ def main_one(csnum):
         torch.nn.Linear(100, output_size),
         torch.nn.LeakyReLU(),
     )
-
+    """
     model = model.to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
