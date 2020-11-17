316e42be3777a7bcfa32fb61cbe91de0c43b6659
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-18 00:06:48 +0800

    update data loader

--- function_training.py
+++ function_training.py
@@ -52,10 +52,10 @@ def validate_train_loop(device, model, optimizer, scheduler, criterion, validate
     if load_data:
         with open ("data.pickle","rb") as f:
             dataset = pickle.load(f)
-        validate_loader, train_loader = data_loader.get_validate_train_loader(dataset, 32)
     result = validate_one_epoch(device, model, criterion, validate_loader)
     print("\rvalidate loss:{} accuracy:{}%".format(*result))
     for epoch in range(num_epoch):
+        validate_loader, train_loader = data_loader.get_validate_train_loader(dataset, 32)
         result = train_one_epoch(device, model, optimizer, criterion, train_loader)
         print("\rtrain epoch:{} loss:{}".format(epoch, result))
         if (epoch + 1) % num_epoch_per_validate == 0:
