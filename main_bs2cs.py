bf4bfe4d85128c4fdffd8999c892b7cfd0d47329
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-18 12:51:23 +0800

    s

--- main_bs2cs.py
+++ main_bs2cs.py
@@ -75,7 +75,7 @@ def main():
     # train
     function_training.validate_train_loop(
         device, model, optimizer, scheduler, criterion, validate_loader, train_loader,
-        num_epoch=1, num_epoch_per_validate=1, state_dict_path="state_dicts/state_dict_bs2cs",load_data=True
+        num_epoch=3, num_epoch_per_validate=1, state_dict_path="state_dicts/state_dict_bs2cs",load_data=True
     )
 
     # apply
