150173cabb79da7cfc9e2c9466bf4f7512f2d514
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-18 23:18:15 +0800

    batch size= 20

--- main_bs2cs.py
+++ main_bs2cs.py
@@ -74,7 +74,7 @@ def main():
     """
     with open ("data.pickle","rb") as f:
         dataset = pickle.load(f)
-    validate_loader, train_loader = data_loader.get_validate_train_loader(dataset, 64)
+    validate_loader, train_loader = data_loader.get_validate_train_loader(dataset, 20)
 
     # train
     ech,loss,ech_a,acc = function_training.validate_train_loop(
