94b50a8693e082753900b6d7832739f38e1753ee
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-18 12:42:05 +0800

    pickle laod data

--- main_bs2cs.py
+++ main_bs2cs.py
@@ -57,7 +57,7 @@ def main():
         data_input_np = np.array(data_json["bands"])[:, hs_indices].flatten().T
         data_label_np = np.array([crystalsystem.crystalsystem_number(data_json["number"]) - 1])
         return data_input_np, data_label_np
-    """
+    
     dataset = data_loader.AnyDataset(
         [f"list/actual/crystalsystem_list_{csnum}.txt" for csnum in range(1, 8)],
         json2inputlabel, validate_size
@@ -66,11 +66,12 @@ def main():
     #validate_loader, train_loader = data_loader.get_validate_train_loader(dataset, 32)
     with open ("data.pickle","wb+") as f:
         pickle.dump(dataset,f)
+
     """
     with open ("data.pickle","rb") as f:
         dataset = pickle.load(f)
     validate_loader, train_loader = data_loader.get_validate_train_loader(dataset, 20)
-
+    """
     # train
     function_training.validate_train_loop(
         device, model, optimizer, scheduler, criterion, validate_loader, train_loader,
