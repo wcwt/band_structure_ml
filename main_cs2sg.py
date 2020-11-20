82f5138dafbe181a81e0982fbbfd45111f749bb7
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-20 12:38:17 +0800

    recover model

--- main_cs2sg.py
+++ main_cs2sg.py
@@ -24,7 +24,7 @@ def main_one(csnum):
 
     cs_sizes = crystalsystem.crystalsystem_sizes()
     output_size = cs_sizes[csnum - 1] - cs_sizes[csnum - 2] + 1 if csnum > 1 else 3
-
+    """
     model = torch.nn.Sequential(
         #torch.nn.LeakyReLU(),
         torch.nn.Linear(len(hs_indices)*num_bands, 64),
@@ -47,7 +47,7 @@ def main_one(csnum):
         torch.nn.Linear(100, output_size),
         torch.nn.LeakyReLU(),
     )
-    """
+    
     model = model.to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
