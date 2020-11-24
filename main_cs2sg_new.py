008352cf67459d5af39cac889022e81ee83fcac5
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-24 12:38:24 +0800

    update plot new function

--- main_cs2sg_new.py
+++ main_cs2sg_new.py
@@ -1,14 +1,27 @@
 import numpy as np
 import pickle
 import torch.nn.functional
-import balance_function as bf
+import matplotlib.pyplot as plt
+
 import data_loader
 import function_training
 import function_list
 
 import crystalsystem
-import matplotlib.pyplot as plt
 
+
+def plot(ech,loss,ech_a,acc):
+    plt.plot(ech,loss)
+    plt.xlabel("num of epoch")
+    plt.ylabel("loss")
+    plt.title("Loss Against Epoch")
+    plt.savefig("loss.png")
+    plt.clf()
+    plt.plot(ech_a,acc)
+    plt.xlabel("num of epoch")
+    plt.ylabel("accuracy")
+    plt.title("Loss Against accuracy")
+    plt.savefig("acc.png")
 
 def main_one(csnum):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -39,6 +52,7 @@ def main_one(csnum):
     )
     """
     model = torch.nn.Sequential(
+        torch.nn.LeakyReLU(),
         torch.nn.Linear(len(hs_indices)*num_bands, 300),
         torch.nn.LeakyReLU(),
         torch.nn.Linear(300, 100),
@@ -52,13 +66,13 @@ def main_one(csnum):
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.75)
     criterion = torch.nn.CrossEntropyLoss()
-    """
+
     # prepare data
     crystal_upper = crystalsystem.spacegroup_index_upper(csnum)
     crystal_lower = crystalsystem.spacegroup_index_lower(csnum)
     crystal_size = crystal_upper - crystal_lower
 
-
+    """
     def json2inputlabel(data_json):
         data_input_np = np.array(data_json["bands"])[:, hs_indices].flatten().T
         sgnum = data_json["number"]
@@ -75,48 +89,21 @@ def main_one(csnum):
 
     with open ("data.pickle","wb+") as f:
         pickle.dump(dataset,f)
+    exit()
     """
 
     with open ("data.pickle","rb") as f:
         dataset = pickle.load(f)
-
-    train_data,train_label = bf.balance(dataset,output_size,outlier=[26])
-    dataset.data_inputs = train_data
-    dataset.data_labels = train_label
-    dataset.update_inform()
-
-
-    """
-    print(len(dataset))
-    y = np.zeros(27)
-    for i in range(len(dataset)):
-        index = int(dataset[i][1])
-        y[index] += 1
-
-    x = range(len(y))
-    plt.bar(x,y)
-    plt.title(f"Total_size = {len(dataset)}")
-    plt.xlabel("space group num within Hex range")
-    plt.ylabel("num of data")
-    plt.savefig("report/original_sample_set")
-    exit()
-    """
-
 
     validate_loader, train_loader = data_loader.get_validate_train_loader(dataset, 32)
 
     # train
     ech,loss,ech_a,acc = function_training.validate_train_loop(
         device, model, optimizer, scheduler, criterion, validate_loader, train_loader,
-        num_epoch=20, num_epoch_per_validate=1, state_dict_path=f"state_dicts/state_dict_cs2sg_{csnum}"
+        num_epoch=5, num_epoch_per_validate=1, state_dict_path=f"state_dicts/state_dict_cs2sg_{csnum}"
     )
 
-    plt.plot(ech,loss)
-    plt.savefig("loss.png")
-    plt.clf()
-    plt.plot(ech_a,acc)
-    plt.savefig("acc.png")
-
+    plot(ech,loss,ech_a,acc)
     # apply
     function_list.append_any_guess_list_files(
         device, model, hs_indices, validate_size, num_group=230,
@@ -126,9 +113,8 @@ def main_one(csnum):
 
 if __name__ == '__main__':
     function_list.create_empty_list_files(230, out_list_path_format="list/guess/spacegroup_list_{}.txt")
-    main_one(6)
-    exit()
+    """
     for i in range(1, 8):
         main_one(i)
-
-    #main_one(6)
+    """
+    main_one(6)
