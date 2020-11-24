3ccb5b176ace49779a5d2a5e9952f45b12943d27
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-24 14:33:29 +0800

    change structure

--- main_cs2sg_new.py
+++ main_cs2sg_new.py
@@ -49,7 +49,7 @@ def main_one(csnum):
 
     cs_sizes = crystalsystem.crystalsystem_sizes()
     output_size = cs_sizes[csnum - 1] - cs_sizes[csnum - 2] + 1 if csnum > 1 else 3
-
+    """
     model = torch.nn.Sequential(
         torch.nn.LeakyReLU(),
         torch.nn.Linear(len(hs_indices)*num_bands, 300),
@@ -59,7 +59,16 @@ def main_one(csnum):
         torch.nn.Linear(100, output_size),
         torch.nn.LeakyReLU(),
     )
-
+    """
+    model = torch.nn.Sequential(
+        #torch.nn.LeakyReLU(),
+        torch.nn.Linear(len(hs_indices)*num_bands, 128),
+        torch.nn.LeakyReLU(),
+        torch.nn.Linear(128, 64),
+        torch.nn.LeakyReLU(),
+        torch.nn.Linear(64, output_size),
+        #torch.nn.LeakyReLU(),
+    )
     model = model.to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
