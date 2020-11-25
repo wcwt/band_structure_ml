7b1f4dbed59fb6d117ca0fd4b02038dc39b6122f
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-25 23:44:23 +0800

    init

--- main_cs2sg_new.py
+++ main_cs2sg_new.py
@@ -11,7 +11,7 @@ import crystalsystem
 import os
 
 dir = "report_graph/"
-folder = dir + "cut_off_70"
+folder = dir + "cut_off_70_advancedSpilt"
 if not os.path.exists(folder):
     os.makedirs(folder)
 folder = folder + "/"
@@ -90,7 +90,7 @@ def main_one(csnum):
     data_in,data_out = bf.data_cutoff(dataset,output_size,cut_off=70)
     data_loader.update_dataset(dataset,data_in,data_out)
     # spilt dataset
-    train_dataset,test_dataset = data_loader.spilt_train_test_dataset(dataset)
+    train_dataset,test_dataset = data_loader.advanced_spilt_train_test_dataset(dataset)
     # balance train part
     #train_in,train_out = bf.balance_avg(train_dataset,output_size)
     #print(f"Before balance:\n{bf.view_count(train_dataset,output_size)}")
