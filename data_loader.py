3acfdb3dbee17785b19af8745bb4b789ecc69fb7
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-25 17:30:15 +0800

    bug fix

--- data_loader.py
+++ data_loader.py
@@ -17,41 +17,41 @@ class AnyDataset(Dataset):
             self.data_labels = []
             self.len = 0
             self.split = 0
-            return 0
-        self.validate_size = validate_size
-        validate_inputs = []
-        validate_labels = []
-        train_inputs = []
-        train_labels = []
-        if not isinstance(in_list_paths, list):  # if only one path is parsed
-            in_list_paths = [in_list_paths]
-        for i, in_list_path in enumerate(in_list_paths):
-            if os.stat(in_list_path).st_size == 0:  # if file is empty
-                continue
-            file_paths = np.loadtxt(in_list_path, "U90", ndmin=1)
-            split = int(validate_size*len(file_paths))
-            for j, file_name in enumerate(file_paths):
-                with open(file_name, "r") as file:
-                    data_json = json.load(file)
-                # json2inputlabel is a function parsed to init, which handles a json dict and give input and label
-                data_input_np, data_label_np = json2inputlabel(data_json)
-                if j <= split:
-                    validate_inputs.append(torch.from_numpy(data_input_np).float())
-                    validate_labels.append(torch.from_numpy(data_label_np).long())
-                else:
-                    train_inputs.append(torch.from_numpy(data_input_np).float())
-                    train_labels.append(torch.from_numpy(data_label_np).long())
-            print("\r\tload: {}/{}".format(i, len(in_list_paths)), end="")
-        print("\rload: {}".format(len(in_list_paths)))
-        if len(train_inputs) == 0:
-            raise OSError("input list file is empty")
-        print(f"train: {len(train_inputs)}")
-        print(f"validate: {len(validate_inputs)}")
-        self.data_inputs = [*validate_inputs, *train_inputs]
-        self.data_labels = [*validate_labels, *train_labels]
+        else:
+            self.validate_size = validate_size
+            validate_inputs = []
+            validate_labels = []
+            train_inputs = []
+            train_labels = []
+            if not isinstance(in_list_paths, list):  # if only one path is parsed
+                in_list_paths = [in_list_paths]
+            for i, in_list_path in enumerate(in_list_paths):
+                if os.stat(in_list_path).st_size == 0:  # if file is empty
+                    continue
+                file_paths = np.loadtxt(in_list_path, "U90", ndmin=1)
+                split = int(validate_size*len(file_paths))
+                for j, file_name in enumerate(file_paths):
+                    with open(file_name, "r") as file:
+                        data_json = json.load(file)
+                    # json2inputlabel is a function parsed to init, which handles a json dict and give input and label
+                    data_input_np, data_label_np = json2inputlabel(data_json)
+                    if j <= split:
+                        validate_inputs.append(torch.from_numpy(data_input_np).float())
+                        validate_labels.append(torch.from_numpy(data_label_np).long())
+                    else:
+                        train_inputs.append(torch.from_numpy(data_input_np).float())
+                        train_labels.append(torch.from_numpy(data_label_np).long())
+                print("\r\tload: {}/{}".format(i, len(in_list_paths)), end="")
+            print("\rload: {}".format(len(in_list_paths)))
+            if len(train_inputs) == 0:
+                raise OSError("input list file is empty")
+            print(f"train: {len(train_inputs)}")
+            print(f"validate: {len(validate_inputs)}")
+            self.data_inputs = [*validate_inputs, *train_inputs]
+            self.data_labels = [*validate_labels, *train_labels]
 
-        self.len = len(self.data_inputs)
-        self.split = int(validate_size * self.len)
+            self.len = len(self.data_inputs)
+            self.split = int(validate_size * self.len)
 
     def __len__(self):
         return self.len
